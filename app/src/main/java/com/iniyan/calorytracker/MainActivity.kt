244235e767f4ad5d014c1aa6f53e577eaa30cb45
iniyanarul <iniyan.m@rapido.bike>
2022-10-08 19:13:39 +0530

    MVVM CleanArchitecture Feature Module Layer and Migration to Kts

---
+++ app/src/main/java/com/iniyan/calorytracker/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.iniyan.calorytracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.iniyan.calorytracker.ui.theme.CaloryTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CaloryTrackerTheme {
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CaloryTrackerTheme {
+        Greeting("Android")
+    }
+}
